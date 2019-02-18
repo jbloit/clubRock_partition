d5dc07373ab8bfb007491f6f42ec5677a4f7a54f
Julien Bloit <julien.bloit@gmail.com>
2019-02-18 12:08:23 +0100

    frontend: socket -> drawanimation connection test. change square size with the note pitch.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var midiInput = new easymidi.Input('IAC Driver Bus 1');
 midiInput.on('noteon', function (params) {
   // do something with msg
   console.log('NOTE ON ' + params['note']);
-  io.emit('chat message', 'NOTE ON ' + params['note']);
+  io.emit('chat message', params['note']);
 });
 
 app.get('/', function(req, res){
